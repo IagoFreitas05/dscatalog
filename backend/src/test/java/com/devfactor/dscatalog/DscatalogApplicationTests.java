38556da7969ad728fead039071e54472a72800a7
Iago Freitas <60206884+IagoFreitas05@users.noreply.github.com>
2023-02-16 12:22:48 +0000

    backend project created

---
+++ backend/src/test/java/com/devfactor/dscatalog/DscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devfactor.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
