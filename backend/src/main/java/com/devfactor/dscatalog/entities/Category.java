eed1edfe439af61b1fdde1ab32d34a537cb1efce
iagofreitas05 <iagofreitas05@gmail.com>
2023-02-22 13:33:01 +0000

    - feature: creating Category resource

--- backend/src/main/java/com/devfactor/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devfactor/dscatalog/entities/Category.java
@@ -1,6 +1,8 @@
-package entities;
+package com.devfactor.dscatalog.entities;
 
-public class Category {
+import java.io.Serializable;
+
+public class Category implements Serializable {
 
     private static final long  serialVersionUID = 1L;
     private Long id;
---
+++ backend/src/main/java/com/devfactor/dscatalog/resources/CategoryResources.java
@@ -0,0 +1,22 @@
+package com.devfactor.dscatalog.resources;
+
+import com.devfactor.dscatalog.entities.Category;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryResources {
+    @GetMapping
+    public ResponseEntity<List<Category>> findAll(){
+        List<Category> list = new ArrayList<>();
+        list.add(new Category(1L, "books"));
+        list.add(new Category(2L, "notebooks"));
+        return ResponseEntity.ok().body(list);
+    }
+}
