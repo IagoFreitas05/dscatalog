45d101dda3c6ab5b7e9440848d34112fe7578bf4
iagofreitas05 <iagofreitas05@gmail.com>
2023-02-23 18:40:21 +0000

    - feature: pagination for the categories findAll method

--- backend/src/main/java/com/devfactor/dscatalog/resources/CategoryResources.java
+++ backend/src/main/java/com/devfactor/dscatalog/resources/CategoryResources.java
@@ -1,16 +1,16 @@
 package com.devfactor.dscatalog.resources;
 
 import com.devfactor.dscatalog.dto.CategoryDTO;
-import com.devfactor.dscatalog.entities.Category;
 import com.devfactor.dscatalog.services.CategoryServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @RequestMapping("/categories")
@@ -18,19 +18,28 @@ public class CategoryResources {
 
     @Autowired
     CategoryServices categoryServices;
+
     @GetMapping
-    public ResponseEntity<List<CategoryDTO>> findAll(){
-        return ResponseEntity.ok().body(categoryServices.findAll());
+    public ResponseEntity<Page<CategoryDTO>> findAll(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction,
+            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy
+
+    ) {
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        Page<CategoryDTO> list = categoryServices.findAllPaged(pageRequest);
+        return ResponseEntity.ok().body(list);
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<CategoryDTO> findById(@PathVariable Long id){
+    public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
         CategoryDTO categoryDTO = categoryServices.findById(id);
         return ResponseEntity.ok().body(categoryDTO);
     }
 
     @PostMapping
-    public ResponseEntity<CategoryDTO> save(@RequestBody CategoryDTO categoryDTO){
+    public ResponseEntity<CategoryDTO> save(@RequestBody CategoryDTO categoryDTO) {
         categoryDTO = categoryServices.save(categoryDTO);
         URI uri = ServletUriComponentsBuilder
                 .fromCurrentRequest()
@@ -42,8 +51,8 @@ public class CategoryResources {
     }
 
     @PutMapping(value = "/{id}")
-    public ResponseEntity<CategoryDTO> update(@RequestBody CategoryDTO categoryDTO, @PathVariable Long id){
-        categoryDTO = categoryServices.update(id ,categoryDTO);
+    public ResponseEntity<CategoryDTO> update(@RequestBody CategoryDTO categoryDTO, @PathVariable Long id) {
+        categoryDTO = categoryServices.update(id, categoryDTO);
         URI uri = ServletUriComponentsBuilder
                 .fromCurrentRequest()
                 .path("/{id}")
@@ -54,7 +63,7 @@ public class CategoryResources {
     }
 
     @DeleteMapping(value = "/{id}")
-    public ResponseEntity<CategoryDTO> delete(@PathVariable Long id){
+    public ResponseEntity<CategoryDTO> delete(@PathVariable Long id) {
         categoryServices.delete(id);
         return ResponseEntity.noContent().build();
     }
--- backend/src/main/java/com/devfactor/dscatalog/services/CategoryServices.java
+++ backend/src/main/java/com/devfactor/dscatalog/services/CategoryServices.java
@@ -8,13 +8,13 @@ import com.devfactor.dscatalog.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityNotFoundException;
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 public class CategoryServices {
@@ -22,23 +22,20 @@ public class CategoryServices {
     private CategoryRepository categoryRepository;
 
     @Transactional(readOnly = true)
-    public List<CategoryDTO> findAll(){
-        List<Category> category = categoryRepository.findAll();
-        return  category
-                .stream()
-                .map(categoryElement -> new CategoryDTO(categoryElement))
-                .collect(Collectors.toList());
+    public Page<CategoryDTO> findAllPaged(PageRequest pageRequest) {
+        Page<Category> category = categoryRepository.findAll(pageRequest);
+        return category.map(categoryElement -> new CategoryDTO(categoryElement));
     }
 
     @Transactional(readOnly = true)
-    public CategoryDTO findById(Long id){
+    public CategoryDTO findById(Long id) {
         Optional<Category> optional = categoryRepository.findById(id);
-        Category category = optional.orElseThrow(() ->new ResourceNotFoundException("Entity not found"));
+        Category category = optional.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
         return new CategoryDTO(category);
     }
 
     @Transactional
-    public CategoryDTO save(CategoryDTO categoryDTO){
+    public CategoryDTO save(CategoryDTO categoryDTO) {
         Category category = new Category();
         category.setName(categoryDTO.getName());
         Category loadedCategory = categoryRepository.save(category);
@@ -52,8 +49,7 @@ public class CategoryServices {
             category.setName(categoryDTO.getName());
             category = categoryRepository.save(category);
             return new CategoryDTO(category);
-        }
-        catch (EntityNotFoundException exception){
+        } catch (EntityNotFoundException exception) {
             throw new ResourceNotFoundException("ID not found " + id);
         }
     }
@@ -61,9 +57,9 @@ public class CategoryServices {
     public void delete(Long id) {
         try {
             categoryRepository.deleteById(id);
-        }catch (EmptyResultDataAccessException exception){
-            throw new ResourceNotFoundException("ID not found "+ id);
-        }catch (DataIntegrityViolationException exception){
+        } catch (EmptyResultDataAccessException exception) {
+            throw new ResourceNotFoundException("ID not found " + id);
+        } catch (DataIntegrityViolationException exception) {
             throw new ResourceDatabaseIntegrityException("Integrity violation");
         }
     }
